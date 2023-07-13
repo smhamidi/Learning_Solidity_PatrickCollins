3894d06be2ce6a2f1561bca526289fe1638228dc
smhamidi <1380hamidi@gmail.com>
2023-07-13 16:22:54 +0330

    chore: forge init

--- FoundrySolidity/SimpleStorage/script/DeploySimpleStorage.s.sol
+++ FoundrySolidity/SimpleStorage/script/DeploySimpleStorage.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 // we have to import script.sol in order to write a solidity contract as an script
-import {Script} from "forge-std/script.sol";
+import {Script} from "forge-std/Script.sol";
 
 // we also have to import our own contract
 import {SimpleStorage} from "../src/SimpleStorage.sol"; // Professional and efficient way to import is like this
@@ -12,8 +12,17 @@ import {SimpleStorage} from "../src/SimpleStorage.sol"; // Professional and effi
 contract DeploySimpleStorage is Script {
     // run(), is the main function to work with in foundry
     function run() external returns (SimpleStorage) {
-        vm.startBroadcast();
+        // vm object belong to foundry, in pure solidity this will not work
+        vm.startBroadcast(); // Anything after this line will be sent to rpc
+        // any transaction that we actully sends is going to be in this block
 
-        vm.stopBroadcast();
+        SimpleStorage simplestorage = new SimpleStorage();
+        // we could also make a SimpleStorage Contract with some value pass to it, the code is as follows
+        // SimpleStorage simplestorage = new SimpleStorage{value: 1 ether}();
+
+        vm.stopBroadcast(); // End of broadcasting
+
+        return simplestorage;
     }
 }
+// head to .env file to see how you can broadcast this script
