bda11d981f04d6e56572db05a34cccd1843ec5cc
smhamidi <1380hamidi@gmail.com>
2023-07-15 21:17:58 +0330

    done with helperConfig, also made a mock contract for our anvil price feed

--- FoundrySolidity/FundMe/script/HelperConfig.s.sol
+++ FoundrySolidity/FundMe/script/HelperConfig.s.sol
@@ -8,6 +8,7 @@ pragma solidity ^0.8.18;
 // e.g. Sepolia ETH/USD, Mainnet ETH/USD
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     // If we are on a local anvil, we deploy mocks
@@ -31,8 +32,38 @@ contract HelperConfig is Script {
         return SepoliaNetwork;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+    // these two number is for us to prevent magical numbers in our code
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_VALUE = 2000e8;
+
+    function getOrCreateAvnilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            // if we already define it then just pass it, do not go through the process again
+            return activeNetworkConfig;
+        }
         // return price feed address for Anvil local network
+        // 1. Deploy the mocks (mock contracts are like dummy contracts)
+        // 2. return the mock address
+        vm.startBroadcast(); // because of this vm keyword, we cannot have this function as pure
+        // MockV3Aggregator mockV3 = new MockV3Aggregator(8, 2000e8); // we hate magical numbers
+        // instead we make our variables like this
+        MockV3Aggregator mockV3 = new MockV3Aggregator(DECIMALS, INITIAL_VALUE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory AnvilNetwork = NetworkConfig({
+            priceFeed: address(mockV3)
+        });
+
+        return AnvilNetwork;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // return price feed address for mainnet network
+        NetworkConfig memory MainetNetwork = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+
+        return MainetNetwork;
     }
 
     NetworkConfig public activeNetworkConfig;
@@ -43,8 +74,10 @@ contract HelperConfig is Script {
         if (block.chainid == 11155111) {
             // you can find these chainids in chainlist.org
             activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAvnilEthConfig();
         }
     }
 }
