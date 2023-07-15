4d4b4bf81ac41ca19234c42e6065b2f2217d700a
smhamidi <1380hamidi@gmail.com>
2023-07-15 19:03:03 +0330

    done with simple helperconfig that will prvide us automatically with the pricefeed address

--- FoundrySolidity/FundMe/script/DeployFundMe.s.sol
+++ FoundrySolidity/FundMe/script/DeployFundMe.s.sol
@@ -4,12 +4,17 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // Anything before vm.startBroadcast() is not a real TX
+        HelperConfig helperConfig = new HelperConfig();
+        address Eth2UsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
 
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(Eth2UsdPriceFeed);
 
         vm.stopBroadcast();
         return fundMe;
--- FoundrySolidity/FundMe/script/HelperConfig.s.sol
+++ FoundrySolidity/FundMe/script/HelperConfig.s.sol
@@ -13,4 +13,38 @@ contract HelperConfig is Script {
     // If we are on a local anvil, we deploy mocks
     // otherwise grab the existing address from the live network
     // With doing this, it does not matter what chain we are dealing with, It WORKS ANYWAY.
+
+    // maybe our config is complex, it can contain for example, price feed address, vrf address and many more
+    // it is a good idea to make config, its own type
+
+    struct NetworkConfig {
+        address priceFeed; // This is the address of ETH/USD price feed in that chain
+    }
+
+    // Our NetworkConfig is an array so you have to specify the storage location in returns argument
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // return price feed address for sepolia network
+        NetworkConfig memory SepoliaNetwork = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return SepoliaNetwork;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // return price feed address for Anvil local network
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    // Now we can detect the network in constructor and set the activeNetworkConfig
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // you can find these chainids in chainlist.org
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
 }
