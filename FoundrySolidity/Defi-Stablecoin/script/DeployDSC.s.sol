169262b96a7b949c99261ea85b91ea11592cc73a
smhamidi <1380hamidi@gmail.com>
2023-08-22 13:54:25 +0330

    OS Changed

--- FoundrySolidity/Defi-Stablecoin/script/DeployDSC.s.sol
+++ FoundrySolidity/Defi-Stablecoin/script/DeployDSC.s.sol
@@ -7,10 +7,20 @@ import {DSCEngine} from "../src/DSCEngine.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        HelperConfig config = new HelperConfig();
+
+        (wethUsdPriceFeed, wbtcUsdPriceFeed, weth, wbtc, deployerKey) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        DSCEngine engine = new DSCEngine();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
         vm.stopBroadcast();
     }
 }
